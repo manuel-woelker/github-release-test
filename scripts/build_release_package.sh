5b497294df6fb588ac06ef3112b38cbd4a20cfd1
Manuel Woelker <github@manuel.woelker.org>
2025-06-22 15:08:30 +0200

    Initial release commit

--- scripts/build_release_package.sh
+++ scripts/build_release_package.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 
 
 PROJECT_NAME=github-release-test
-
+VERSION=${1:-no-version}
 TARGET=${RUSTTARGET:-x86_64-pc-windows-msvc}
 
 echo "Building hyperlit binary"
@@ -16,9 +16,9 @@ cargo build --release --target $TARGET --frozen
 if [[ $TARGET == *"windows"* ]]; then
   echo "Creating zip file"
   rm -f "$PROJECT_NAME-$TARGET.zip"
-  zip -j -r "$PROJECT_NAME-$TARGET.zip" "./target/$TARGET/release/$PROJECT_NAME.exe"
+  zip -j -r "$PROJECT_NAME-$VERSION-$TARGET.zip" "./target/$TARGET/release/$PROJECT_NAME.exe"
 else
   echo "Creating tar.gz file"
   rm -f "$PROJECT_NAME-$TARGET.tar.gz"
-  tar -zcvf "$PROJECT_NAME-$TARGET.tar.gz" "./target/$TARGET/release/$PROJECT_NAME"
+  tar -zcvf "$PROJECT_NAME-$VERSION-$TARGET.tar.gz" "./target/$TARGET/release/$PROJECT_NAME"
 fi
