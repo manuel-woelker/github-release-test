ac33f2a56390f4af5d6e2a7923dbae8de5656423
Manuel Woelker <github@manuel.woelker.org>
2025-06-22 14:56:09 +0200

    Initial release commit

--- scripts/build_release_package.sh
+++ scripts/build_release_package.sh
@@ -11,9 +11,16 @@ TARGET=${RUSTTARGET:-x86_64-pc-windows-msvc}
 echo "Building hyperlit binary"
 echo "Target: $TARGET"
 
-cargo build --release --target $TARGET
+cargo build --release --target $TARGET --frozen
 
-
-echo "Creating zip file"
-rm -f "$PROJECT_NAME-$TARGET.zip"
-zip -j -r "$PROJECT_NAME-$TARGET.zip" "./target/$TARGET/release/$PROJECT_NAME.exe"
+// zip if on windows i.e. $TARGET contains windows
+if [[ $TARGET == *"windows"* ]]; then
+  echo "Creating zip file"
+  rm -f "$PROJECT_NAME-$TARGET.zip"
+  zip -j -r "$PROJECT_NAME-$TARGET.zip" "./target/$TARGET/release/$PROJECT_NAME.exe"
+  exit 0
+else
+  echo "Creating tar.gz file"
+  rm -f "$PROJECT_NAME-$TARGET.tar.gz"
+  tar -zcvf "$PROJECT_NAME-$TARGET.tar.gz" "./target/$TARGET/release/$PROJECT_NAME"
+fi
